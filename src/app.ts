35d1fddc23adfbcfb404918b932b2e1458a55453
Alexander Guerrero <alex.guelu@gmail.com>
2024-03-10 15:29:41 -0500

    75. Tarea - Impresión de consola y archivo - Mi solución

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,34 @@
 
-const message = 'Hello world!!!';
+// Tarea 
+// tabla de multiplicar del 1 al 10 
+// grabar en el archivo de salida 
+// path: outputs/tabla-5.txt 
 
-console.log( message );
+const multiplicationTable = ( num: number ): string => {
+
+    let res: string = '';
+
+    res += '=================\n';
+    res += `   Tabla del ${num}   \n`;
+    res += '=================\n\n';
+
+    for (let i = 1; i <= 10; i++) {
+        res += `${ num } x ${ i } = ${ num * i }\n`;
+    }
+
+    return res;
+};
+
+const writeToFile = ( location: string, content: string ): void => {
+    const fs = require('fs');
+
+    try {
+        fs.writeFileSync(location, content);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const tableX = multiplicationTable( 5 );
+console.log(tableX);
+writeToFile( 'outputs/tabla-5.txt', tableX );
