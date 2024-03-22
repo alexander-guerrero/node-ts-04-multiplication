2ad8f84b0750146397eda58ebe50ae3cc9f16ecc
Alexander Guerrero <alex.guelu@gmail.com>
2024-03-22 17:20:41 -0500

    96. Pruebas - SaveFile UseCase custom values

--- src/domain/use-cases/save-file.use-case.test.ts
+++ src/domain/use-cases/save-file.use-case.test.ts
@@ -3,11 +3,12 @@ import fs from "fs";
 
 describe('SaveFileUseCase', () => {
 
-    const outputPath = 'outputs/table.txt';
+    let outputPath = 'outputs/table.txt';
 
     // Ciclo de vida de las pruebas (Jest Lifecycle) 
     // beforeAll, beforeEach, afterEach, afterAll 
     afterEach(() => {
+        // console.log('outputPath', outputPath);
         fs.rmSync(outputPath, { recursive: true });
     });
     
@@ -28,6 +29,28 @@ describe('SaveFileUseCase', () => {
         expect( fileExists ).toBe( true );
         expect( fileContent ).toBe( options.fileContent );
 
+    });
+
+    test('should save file with custom values', () => {
+
+        const objSaveFile = new SaveFile();
+        const options = {
+            fileContent: 'custom content',
+            filePath: 'outputs/my/custom/path',
+            fileName: 'my-custom-name'
+        }
+        outputPath = `${options.filePath}/${options.fileName}.txt`;
+        
+        const result = objSaveFile.execute(options);
+        const fileExists = fs.existsSync(outputPath);
+        // console.log(fileExists);
+        const fileContent = fs.readFileSync(outputPath, { encoding: 'utf-8' });
+        // console.log(fileContent);
+
+        expect( result ).toBe( true );
+        expect( fileExists ).toBe( true );
+        expect( fileContent ).toBe( options.fileContent );
+        
     });
 
 });
