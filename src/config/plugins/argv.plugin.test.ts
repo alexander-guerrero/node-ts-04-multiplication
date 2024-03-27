bf0740e2f99bfcf1167708610c79dd780892b97f
Alexander Guerrero <alex.guelu@gmail.com>
2024-03-26 20:05:47 -0500

    99. Pruebas - Argv valores personalizados

--- src/config/plugins/argv.plugin.test.ts
+++ src/config/plugins/argv.plugin.test.ts
@@ -10,13 +10,20 @@ const runCommand = async ( args: string[] ) => {
 
 describe('Test argv.plugin.ts', () => {
     
+    const originalArgv = process.argv;
+
+    beforeEach(() => {
+        process.argv = originalArgv;
+        jest.resetModules();
+    });
+
     test('should return default values', async () => {
         
         const argv = await runCommand( ['-b', '5'] );
 
         // console.log('process.argv', process.argv);
-        console.log('argv', argv);
-        
+        // console.log('argv', argv);
+
         expect( argv ).toEqual( expect.objectContaining({
             b: 5,
             l: 10,
@@ -25,6 +32,23 @@ describe('Test argv.plugin.ts', () => {
             d: 'outputs',
         }) );
         
+    });
+
+    test('should return configuration with custom values', async () => {
+        
+        const argv = await runCommand( ['-b', '7', '-l', '12', '-s', 'true', '-n', 'custom-name', '-d', 'outputs/custom-path'] );
+
+        // console.log('process.argv t2', process.argv);
+        // console.log('argv t2', argv);
+
+        expect( argv ).toEqual( expect.objectContaining({
+            b: 7,
+            l: 12,
+            s: true,
+            n: 'custom-name',
+            d: 'outputs/custom-path'
+        }) );
+
     });
 
 });
