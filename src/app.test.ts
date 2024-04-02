5837c4b1fb9c39b56a21fab711ae95823265499e
Alexander Guerrero <alex.guelu@gmail.com>
2024-04-02 18:29:57 -0500

    104. Pruebas - App.ts

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,9 +1,29 @@
+// process.argv = ['node', 'app.ts', '-b', '10'];
+// import './app';
+// Primero agregar las variables en argv, y luego importar. Funciona, pero se verá otra forma :) 
 
-describe('App', () => {
+import { ServerApp } from "./presentation/server-app";
+
+describe('Test app.ts', () => {
     
-    test('should be true', () => {
+    test('should call ServerApp.run with values', async () => {
+
+        // Arrange 
+        const serverAppRunMock = jest.fn();
+        ServerApp.run = serverAppRunMock;
+        process.argv = ['node', 'app.ts', '-b', '10', '-l', '5', '-s', '-n', 'test-file', '-d', 'outputs/test-path'];
         
-        expect(true).toBe(true);
+        // Act 
+        await import('./app');
+
+        // Assert 
+        expect( serverAppRunMock ).toHaveBeenCalledWith({
+            base: 10, 
+            limit: 5, 
+            showTable: true, 
+            name: 'test-file', 
+            destination: 'outputs/test-path'
+        });
 
     });
 
